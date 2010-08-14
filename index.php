783cb10c0032365854b4301891cb182c4dbc1411
David Phillips <david@acz.org>
2010-08-14 11:12:44 -0700

    Integrate UI with Facebook Connect

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml"
       xmlns:fb="http://www.facebook.com/2008/fbml">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <title>ImHeere</title>
-  </head>
-  <body>
-    <h1>ImHeere</h1>
-    <p><fb:login-button autologoutlink="true" show-faces="true" width="300"></fb:login-button></p>
-    <p><fb:like></fb:like></p>
+<head>
+  <meta name="viewport" content="initial-scale=1.0, user-scalable=no" /> 
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <title>Who is here?????</title>
+  <link rel="stylesheet" type="text/css" href="master.css" />
+</head>
+<body>
+  <div>
+    <div class="main-top">
+    <div>Who is here?????</div>
+  </div>
+  <div class="main-middle">
+    <h1>Join Now, It's Free</h1>
+    <div class="content">
+    <p>Ever wondered if the person next to you has the same interests as you?</p><br />
+    <p>Sign Up to find people at your location that meet your interests.</p>
+    </div>
+    <br />
+    <p><fb:login-button autologoutlink="true" width="300"></fb:login-button></p>
 
     <div id="fb-root"></div>
     <script>
@@ -19,11 +30,15 @@
       (function() {
         var e = document.createElement('script');
         e.type = 'text/javascript';
-        e.src = document.location.protocol +
-          '//connect.facebook.net/en_US/all.js';
+        e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
         e.async = true;
         document.getElementById('fb-root').appendChild(e);
       }());
     </script>
-  </body>
+
+    <br />
+    <div class="sign-up-btn1"><a href="#" onclick="alert('TODO')">Twitter</a></div>
+  </div>
+</div>
+</body>
 </html>
