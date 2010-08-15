5a3c4b6fb58eb42f76165bbb163a922944461a77
David Phillips <david@acz.org>
2010-08-15 14:56:56 -0700

    Fix graffiti

--- graffiti.php
+++ graffiti.php
@@ -25,15 +25,16 @@ if (isset($_POST["imgdata"])) {
 <html xmlns="http://www.w3.org/1999/xhtml"
       xmlns:fb="http://www.facebook.com/2008/fbml">
 <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>SweetGeo</title>
   <link rel="stylesheet" type="text/css" href="master.css" />
- 
 <script type="text/javascript" src="lzwencoder.js"></script>
 <script type="text/javascript" src="neuquant.js"></script>
 <script type="text/javascript" src="gifencoder.js"></script>
 <script type="text/javascript" src="b64.js"></script>
   </head>
-
-  <body>
+  <body onload="setTimeout(function () {window.scrollTo(0,1);}, 0);" style="background: gray">
 <div>
   <div class="main-top">
     <div>SweetGeo</div>
@@ -41,18 +42,17 @@ if (isset($_POST["imgdata"])) {
   <div class="header-left"><a href="location.php">Back</a></div>
   <div class="header-right"><a href="#" onclick="render_timer(); return false;">Post</a></div>
 </div>
-<div class="main-middle-textBox">
-  <? if (isset($error)): ?>
-  <div class="status_error"><?=$error?></div>
-  <? endif; ?>
-  
-  
-   <canvas id="paint_area"   style="align=center;border:1px solid blue"></canvas>
-    
+<div align="center">
+<canvas id="paint_area" style="align=center;border:1px solid black; margin: 20px 8px 10px 8px;"></canvas>
+<br />
+<button onclick="clear_up()">Clear Autograph</button>
+</div>
+
     <script>
 	var ctx = document.getElementById('paint_area').getContext('2d');
 	var canvas_ = document.getElementById('paint_area');
-    canvas_.width= document.body.offsetWidth;
+    canvas_.width = 250;
+    canvas_.height = 100;
 	function render_timer()
 	{
 		  var encoder = new GIFEncoder();
@@ -73,17 +73,17 @@ if (isset($_POST["imgdata"])) {
 	  var storeX = 0.0;
 	  var storeY = 0.0;
 	  ctx.strokeWidth=3.0;
-	  ctx.strokeStyle="#FFCC00";
+	  ctx.strokeStyle="#1C1C52";
 	  ctx.fillStyle= "#FFFFFF";
 	  ctx.fillRect(0,0,document.getElementById('paint_area').width,document.getElementById('paint_area').height);
       ctx.canvas.addEventListener('mousedown', function(event) { 
-	  storeX=event.clientX;
-	  storeY=event.clientY;
+	  storeX=event.clientX - canvas_.offsetLeft;
+	  storeY=event.clientY - canvas_.offsetTop;
 	  down = true; }, false)
 	  ctx.canvas.addEventListener("touchstart", function(event){
 	  event.preventDefault();
-      storeX=event.clientX;
-	  storeY=event.clientY;
+	  storeX=event.clientX - canvas_.offsetLeft;
+	  storeY=event.clientY - canvas_.offsetTop;
 	  down = true;}, false);
 	  
 	  ctx.canvas.addEventListener("touchend", function(event) { 
@@ -95,27 +95,30 @@ if (isset($_POST["imgdata"])) {
 	  ctx.canvas.addEventListener("touchmove", function(event) { 
 	  event.preventDefault();
 	   if (down) {
+	     var pageX = event.targetTouches[0].pageX - canvas_.offsetLeft;
+	     var pageY = event.targetTouches[0].pageY - canvas_.offsetTop;
 		 ctx.beginPath();
 		 ctx.moveTo(storeX, storeY);
-		 ctx.lineTo(event.targetTouches[0].pageX, event.targetTouches[0].pageY);
+		 ctx.lineTo(pageX, pageY);
 		 ctx.closePath();
 		 ctx.stroke();
-		 storeX = event.targetTouches[0].pageX;
-		 storeY = event.targetTouches[0].pageY; 
+		 storeX = pageX;
+		 storeY = pageY; 
 		 }
 	  }, false)
 	  
 	  ctx.canvas.addEventListener('mousemove', function(event) { 
         if (down) {
-
+         var clientX = event.clientX - canvas_.offsetLeft;
+         var clientY = event.clientY - canvas_.offsetTop;
 		 ctx.beginPath();
 		 ctx.moveTo(storeX, storeY);
-		 ctx.lineTo(event.clientX, event.clientY);
+		 ctx.lineTo(clientX, clientY);
          	
 		ctx.closePath();
 		 ctx.stroke();
-      storeX=event.clientX;
-	  storeY=event.clientY;
+      storeX=clientX;
+	  storeY=clientY;
 		 }
       }, false);
 	  
@@ -126,14 +129,10 @@ if (isset($_POST["imgdata"])) {
 	  }
 
     </script>
-	<!--<div class="header-left"><a href="location.php">Back</a></div>
-  <div class="header-right"><a href="#" onclick="submit(); return false;">Post</a></div>-->
+
  <form id="form" action="<?=$self?>" method="post">
-    <!--<input type="button" value="Submit" onclick="render_timer()"/>-->
     <input type="hidden" name="imgdata" id='imgdata' />
 	<input type="hidden" name="location" value="<?=$location?>" />
-	<input type="button" value="Clear" onclick="clear_up()"/>
-  </form>
-  
+  </form> 
 </body>
 </html>
