c3a400b5d2e9f877ce4021169ecb3eb3b731f38d
Nik Hodgkinson <nik@ubuntu-1010.(none)>
2010-08-15 07:12:34 -0700

    Update to locations HTML.

---
+++ resources/body_content/locations.php
@@ -0,0 +1,44 @@
+Ê Ê Ê Ê<div>
+ Ê Ê Ê Ê Ê Ê<div class="main-top">
+ Ê Ê Ê Ê Ê Ê Ê Ê<div>
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê<img alt="Company Name" src="">
+ Ê Ê Ê Ê Ê Ê Ê Ê</div>
+ Ê Ê Ê Ê Ê Ê Ê Ê<div class="header-right">
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê<a href="#">Refresh</a>
+ Ê Ê Ê Ê Ê Ê Ê Ê</div>
+ Ê Ê Ê Ê Ê Ê</div>
+ Ê Ê Ê Ê Ê Ê<!-- end top nav bar -->
+ Ê Ê Ê Ê Ê Ê<!-- begin search -->
+ Ê Ê Ê Ê Ê Ê<div class="main-middle-search">
+ Ê Ê Ê Ê Ê Ê Ê Ê<div class="search_form">
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê<form id="search_form" action="" method="post">
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê<input class="search_form_field" onfocus="clearMe(this)" onblur="this.value=!this.value?'Name*':this.value;" onclick="this.value='';" name="search-field" value="Enter Location Name" type="text"><input class="search_form_submit" name="search-btn" id="button" value="Find" type="submit">
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê</form>
+ Ê Ê Ê Ê Ê Ê Ê Ê</div><!-- end search -->
+ Ê Ê Ê Ê Ê Ê Ê Ê
+ Ê Ê Ê Ê Ê Ê Ê Ê<!-- begin locations -->
+ Ê Ê Ê Ê Ê Ê Ê Ê <div class="title-line">
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê <p>
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Locations Near You
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê </p>
+ Ê Ê Ê Ê Ê Ê Ê Ê </div>
+ Ê Ê Ê Ê Ê Ê Ê Ê <!-- location container -->
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê<div class="loc">
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê<div class="location_left_side">
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê<img src="" title="">
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê</div>
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê<div class="location_right_side">
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê<div class="location_title_side">
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê ÊLocation Title
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê</div>
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê<div class="location_distance_side">
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê ÊDistance
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê</div>
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê Ê</div>
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê</div>
+ Ê Ê Ê Ê Ê Ê Ê<!-- end location container -->
+ Ê Ê Ê Ê Ê Ê Ê Ê Ê
+ Ê Ê Ê Ê Ê Ê Ê Ê<!-- end locations -->
+ Ê Ê Ê Ê Ê Ê Ê Ê
+ Ê Ê Ê Ê Ê Ê</div>
+ Ê Ê Ê Ê</div>
