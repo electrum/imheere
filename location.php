2a6fbb070f3e88164955528dac01b26a4e4ff1fd
David Phillips <david@acz.org>
2010-08-15 11:11:03 -0700

    Revert "Formatting cleanup."
    This reverts commit 7d7669531a8516302ffb80bb2eb21114e2976a73.
    
    Conflicts:
    
    	location.php

--- location.php
+++ location.php
@@ -9,69 +9,67 @@ $self = $_SERVER["PHP_SELF"];
 $searchhelp = "Enter Location Name";
 $searchtext = $searchhelp;
 if (isset($_GET["latlng"])) {
-  list($lat, $lng) = explode(",", $_GET["latlng"]);
-  $spots = spots($lat, $lng);
+    list($lat, $lng) = explode(",", $_GET["latlng"]);
+    $spots = spots($lat, $lng);
 }
 else if (isset($_GET["q"])) {
-  $searchtext = $_GET["q"];
-  $geo = geocode($searchtext);
-  if ($geo === false) {
-    $geoerror = "Error geocoding location. Please try again.";
-  }
-  else if (count($geo) == 0) {
-    $geoerror = "We could not understand the location.";
-  }
-  else if (count($geo) > 1) {
-    $geoerror = "Location returned multiple results. Please be more specific.";
-  }
-  else {
-    list($lat, $lng) = $geo[0];
-    $spots = spots($lat, $lng);
-  }
+    $searchtext = $_GET["q"];
+    $geo = geocode($searchtext);
+    if ($geo === false) {
+        $geoerror = "Error geocoding location. Please try again.";
+    }
+    else if (count($geo) == 0) {
+        $geoerror = "We could not understand the location.";
+    }
+    else if (count($geo) > 1) {
+        $geoerror = "Location returned multiple results. Please be more specific.";
+    }
+    else {
+        list($lat, $lng) = $geo[0];
+        $spots = spots($lat, $lng);
+    }
 }
 ?><?=html_header()?>
-<script type="text/javascript">
+  <script type="text/javascript">
 function geo() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(pos) {
-      lat = pos.coords.latitude;
-      lng = pos.coords.longitude;
-      window.location = '<?=$self?>?latlng=' + lat + ',' + lng;
-    }, function(error) {
-      var err = error.code;
-      switch (error.code) {
-        case error.PERMISSION_DENIED: err = "PERMISSION_DENIED"; break;
-        case error.POSITION_UNAVAILABLE: err = "POSITION_UNAVAILABLE"; break;
-        case error.TIMEOUT: err = "TIMEOUT"; break;
-      }
-      alert("error finding position: " + err);
-    });
-  }
-  else {
-    alert("Sorry, your browser does not support geo location")
-  }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(pos) {
+            lat = pos.coords.latitude;
+            lng = pos.coords.longitude;
+            window.location = '<?=$self?>?latlng=' + lat + ',' + lng;
+        }, function(error) {
+            var err = error.code;
+            switch (error.code) {
+            case error.PERMISSION_DENIED: err = "PERMISSION_DENIED"; break;
+            case error.POSITION_UNAVAILABLE: err = "POSITION_UNAVAILABLE"; break;
+            case error.TIMEOUT: err = "TIMEOUT"; break;
+            }
+            alert("error finding position: " + err);
+        });
+    }
+    else {
+        alert("Sorry, your browser does not support geo location")
+    }
 }
 <? if ((!isset($_GET["q"])) && (!isset($_GET["latlng"]))): ?>
 geo();
 <? endif; ?>
-</script>
-</head>
-<body>
-  <div class="main-top">
-    <div>
-      <img alt="Company Name" src="" />
+  </script>
+  </head>
+  <body>
+    <div class="main-top">
+      <div>
+        <img alt="Company Name" src="" />
+      </div>
+      <div class="header-left-fb">
+        <?=facebook_div()?>
+        <fb:login-button autologoutlink="true"></fb:login-button>
+      </div>
+      <div class="header-right"><a href="<?=$self?>">Refresh</a></div>
     </div>
-    <div class="header-left-fb">
-      <?=facebook_div()?>
-      <fb:login-button autologoutlink="true"></fb:login-button>
-    </div>
-    <div class="header-right">
-      <a href="<?=$self?>">Refresh</a>
-    </div>
-  </div>
-  <div class="main-middle-search">
-    <div class="search_form">
-      <? if (isset($geoerror)): ?>
+    <div class="main-middle-search">
+      <div class="search_form">
+        <? if (isset($geoerror)): ?>
         <div class="geocode_error"><?=$geoerror?></div>
         <? endif; ?>
         <form id="search_form" action="<?=$self?>" method="get">
@@ -83,13 +81,13 @@ geo();
             name="q"
             value="<?=htmlspecialchars($searchtext)?>"
             type="text"/>
-          <input class="search_form_submit" value="Find" type="submit"/>
-        </form>
-      </div>
+        <input class="search_form_submit" value="Find" type="submit"/>
+      </form>
+     </div>
       <? if (isset($spots)): ?>
       <div class="title-line">
-        <p>Locations Near You</p>
-      </div>
+       <p>Locations Near You</p>
+     </div>
       <div id='loc_container'>
       <? foreach ($spots as $id => $i): ?>
         <div class="loc">
@@ -101,9 +99,11 @@ geo();
             <div class="location_title_side"><?=htmlspecialchars($i["name"])?></div>
             <div class="location_distance_side"><?=round($i["distance"])?> meters</div>
           </div>
-        <? endforeach; ?>
+          </a>
+        </div>
+      <? endforeach; ?>
+      <? endif; ?>
       </div>
-    <? endif; ?>
-  </div>
-</body>
+    </div>
+  </body>
 </html>
