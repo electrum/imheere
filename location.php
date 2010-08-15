d2c58a8d4df109ac905cc66b6841692b0b1adaba
Nik Hodgkinson <nikh@ning.com>
2010-08-15 01:38:54 -0700

    Removed debug code.

--- location.php
+++ location.php
@@ -29,14 +29,7 @@ else if (isset($_GET["q"])) {
         $spots = spots($lat, $lng);
     }
 }
-?><!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml"
-      xmlns:fb="http://www.facebook.com/2008/fbml">
-    <head>
-        <meta name="viewport" content="initial-scale=1.0, user-scalable=no" /> 
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title>SweetGeo</title>
-        <link href="resources/masterStyle.css" rel="stylesheet" type="text/css" />
+?><?=html_header()?>
   <script type="text/javascript">
 function geo() {
     if (navigator.geolocation) {
